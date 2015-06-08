75d7118427176dcdec7e452d262b2a943eea5440
kathybarahona03 <katbara03@gmail.com>
2015-06-07 17:34:04 -0700

    POST y GET de products ready!

--- app/public/app.js
+++ app/public/app.js
@@ -1,47 +1,32 @@
 
-
-(function(){
 	var app = angular.module('flvStore',[]);
-	app.controller('flvStoreController',function($scope,$http){
-		$scope.formData = {};
-		$http.get('/api/products')
-		   .success(function(data){
-			$scope.products=data;
-			console.log(data)
-		})
-		 .error(function(data){
-		   	console.log('Error: ' + data);
-		   });
-		 $scope.createProduct = function(){
-		 	$http.post('/api/products', $scope.formData)
-		 	.success(function(data){
-		 		$scope.formData = {};
-		 		$scope.products = data;
-		 		console.log(data);
-		 	})
-		 	.error(function(data){
-		 		console.log("Error:" + data);
-		 	});
-		 };
-		 $scope.deleteProduct = function(id){
-		 	$http.delete('api/products/' + id)
-		 	.success(function(data){
-		 		$scope.products = data;
-		 		console.log(data);
-		 	})
-		 	.error(function(data){
-		 		console.log('Error:' + data);
-		 	});
-		 };
+	app.controller('flvCtrl',['$scope', '$http',function($scope,$http){
+			$scope.product = {};
+			 console.log("Hello World from controller");
+			 refresh = function(){
+				$http.get('/products').success(function(data){
+				console.log("I got the data I requested");
+				$scope.products=data;
+                
+			});
+	    };
+		$scope.addProduct = function(){
+			console.log($scope.product);
+			$http.post('/products', $scope.product).success(function(response){
+				console.log(response);
+			});
 
-	});
+		};
+
+	}]);
 	app.controller('TabController',function(){
 		this.tab = 1;
 		this.isSet = function(checkout){
 			 return this.tab === checkout;
 		};
 		this.setTab = function(number){
+			if(number===2)
+				refresh();
 			this.tab = number;
 		}
 	});
-})();
